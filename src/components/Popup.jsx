d28523e39c2164a88e86af20262bfcfafe86d2cc
devang sharma <sharmadevang7861@gmail.com>
2023-07-31 20:34:30 +0530

    email_validation

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,30 +1,115 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import styles from './Popup.module.css'
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import styles from './Popup.module.css';
 import { Dialog } from 'primereact/dialog';
-
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
 
 const Popup = () => {
-
     const [visible, setVisible] = useState(false);
+    const [userInfo, setUserInfo] = useState(null);
+    const toast = useRef(null);
 
     useEffect(() => {
         setTimeout(() => {
-            setVisible(true)
-        }, 3000)
-    }, [])
+            setVisible(true);
+        }, 3000);
+    }, []);
+
+    const showSuccess = (e) => {
+        toast.current.show({ severity: 'success', summary: 'Success', detail: e?e:'Form Submitted', life: 3000 });
+    };
+
+    const showError = (e) => {
+        toast.current.show({severity:'error', summary: 'Error', detail: e, life: 3000});
+    };
+
+    const isValidEmail = (str) => {
+        // Regular expression for email validation
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+        return emailRegex.test(str);
+    }
+
+
+    const handleSubmit = () => {
+
+        const inputString = userInfo?.email;
+        if (isValidEmail(inputString)) {
+        } else {
+            showError('Email not valid please re-enter')
+            console.log("Invalid email address.");
+            return
+        }
+
+
+        console.log('Form submitted!', userInfo);
+        setVisible(false)
+        showSuccess()
+    };
+
+    const nameChangeHandler = (e) => {
+        const obj = {
+            name: e.target.value,
+            phone: userInfo?.phone ? userInfo.phone : null,
+            email: userInfo?.email ? userInfo.email : null
+        }
+        setUserInfo(obj)
+    }
+
+    const phoneChangeHandler = (e) => {
+        const obj = {
+            name: userInfo?.name ? userInfo.name : null,
+            phone: e.target.value,
+            email: userInfo?.email ? userInfo.email : null
+        }
+
+        setUserInfo(obj)
+    }
+
+    const emailChangeHandler = (e) => {
+        const obj = {
+            name: userInfo?.name ? userInfo.name : null,
+            phone: userInfo?.phone ? userInfo.phone : null,
+            email: e.target.value
+        }
+        setUserInfo(obj)
+    }
 
     return (
         <Fragment>
-            <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
-                <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+
+            <Toast ref={toast} />
+
+            <Button onClick={()=> setVisible(true)}>click</Button>
+
+            <Dialog
+                header="Form"
+                visible={visible}
+                onHide={() => setVisible(false)}
+                closable={false}
+                // style={{ width: '50%' }}
+                className={styles['popup']}
+            >
+                <div className={styles['parent-container']}>
+                    <div className={styles['form-group']}>
+                        {/* <label>Name:</label> */}
+                        <input type="text" placeholder="Enter your name" onChange={(e) => nameChangeHandler(e)} className={styles['input']} />
+                    </div>
+                    <div className={styles['form-group']}>
+                        {/* <label>Phone:</label> */}
+                        <input type="text" placeholder="Enter your phone number" onChange={(e) => phoneChangeHandler(e)} className={styles['input']} />
+                    </div>
+                    <div className={styles['form-group']}>
+                        {/* <label>Email:</label> */}
+                        <input type="email" placeholder="Enter your email address" onChange={(e) => emailChangeHandler(e)} className={styles['input']} />
+                    </div>
+                    <div className={styles['form-group']}>
+                        <Button onClick={handleSubmit}>Submit</Button>
+                    </div>
+                </div>
             </Dialog>
         </Fragment>
-    )
-}
+    );
+};
 
-export default Popup
+export default Popup;
