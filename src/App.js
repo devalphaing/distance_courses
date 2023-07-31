4b8512a713715780f0a4f37bc660d43bdd808c77
devang sharma <sharmadevang7861@gmail.com>
2023-07-31 19:16:02 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import { Fragment } from 'react';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Fragment>
+      <Home />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+const Home = ()=> {
+  return (
+    <Fragment>
+        home page
+    </Fragment>
+  )
+}
+export default Home
